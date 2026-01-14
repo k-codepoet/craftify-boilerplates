997b04c977fee9e1bc6187a1ea0c9d469448f00b
choigawoon <choigawoon@gmail.com>
2026-01-14 11:43:45 +0000

    tanstack 시리즈 추가

---
+++ web/tanstack-start-ssr/vite.config.ts
@@ -0,0 +1,21 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart({ customViteReactPlugin: true }),
+    viteReact(),
+  ],
+})
